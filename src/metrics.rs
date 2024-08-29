1b1fb9e1efd6bc95c2ef480fe8a6911ff2edf753
v-wangtianyi <v-wangtianyi@360.cn>
2024-08-29 16:34:44 +0800

    concurrent hashmap

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,22 +1,22 @@
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Mutex, RwLock};
 use anyhow::{anyhow, Result};
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    pub date: Arc<Mutex<HashMap<String, i64>>>,
+    pub date: Arc<RwLock<HashMap<String, i64>>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            date: Arc::new(Mutex::new(HashMap::new())),
+            date: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
         let mut date = self.date
-            .lock()
+            .write()
             .map_err(|e| anyhow!(e.to_string()))?;
         let counter = date.entry(key.into()).or_insert(0);
         *counter += 1;
@@ -25,7 +25,7 @@ impl Metrics {
 
     pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
         let date = self.date
-            .lock()
+            .read()
             .map_err(|e| anyhow!(e.to_string()))?
             .clone();
         Ok(date)
